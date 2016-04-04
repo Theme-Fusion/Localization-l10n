e417c3b54852ea16f7c296984cf1c960eb2b8d61
Aristeides Stathopoulos <aristath@gmail.com>
2016-04-04 19:38:23 +0300

    just indentation fixes, nothing more.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,71 +1,73 @@
 module.exports = function(grunt) {
+
 	// Project configuration.
-	
-grunt.initConfig({
-  msgInitMerge: {
-    theme: {
-      src: ['Avada/Avada.pot'],
-      options: {
-          locales: ['ca', 'bg_BG', 'cs_CZ', 'da_DK', 'de_DE','el', 'es_ES', 'fa', 'fa_IR', 'fi', 'fr_FR', 'he', 'hr_HR', 'hu_HU', 'it_IT', 'ja', 'ko', 'mk_MK', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'pt_PT', 'ro', 'ru_RU', 'sv_SE', 'tr_TR', 'uk_UA', 'zh_CN', 'zh_TW' ],
-          poFilesPath: 'Avada/<%= locale%>.po',
-		  msgInit : {
-				cmd : 'msginit',
-				opts : {}
+	grunt.initConfig({
+		msgInitMerge: {
+			theme: {
+				src: ['Avada/Avada.pot'],
+				options: {
+					locales: ['ca', 'bg_BG', 'cs_CZ', 'da_DK', 'de_DE','el', 'es_ES', 'fa', 'fa_IR', 'fi', 'fr_FR', 'he', 'hr_HR', 'hu_HU', 'it_IT', 'ja', 'ko', 'mk_MK', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'pt_PT', 'ro', 'ru_RU', 'sv_SE', 'tr_TR', 'uk_UA', 'zh_CN', 'zh_TW' ],
+					poFilesPath: 'Avada/<%= locale%>.po',
+					msgInit: {
+						cmd: 'msginit',
+						opts: {}
+					},
+					msgMerge: {
+						cmd: 'msgmerge',
+						opts: {
+							'no-fuzzy-matching': true,
+							'backup': 'none',
+						}
+					}
+				}
 			},
-			msgMerge : {
-				cmd : 'msgmerge',
-				opts : {'no-fuzzy-matching' : true, 'backup' : 'none'}
+			plugin: {
+				src: ['fusion-core/fusion-core.pot'],
+				options: {
+					locales: ['de_DE','es_ES', 'fi', 'fr_FR', 'he', 'it_IT', 'ja', 'ko', 'mk_MK', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'ru_RU'],
+					poFilesPath: 'fusion-core/fusion-core-<%= locale%>.po',
+					msgInit: {
+						cmd: 'msginit',
+						opts: {}
+					},
+					msgMerge: {
+						cmd: 'msgmerge',
+						opts: {
+							'no-fuzzy-matching': true,
+							'backup': 'none',
+						}
+					}
+				}
 			}
-      }
-    },
-	plugin: {
-      src: ['fusion-core/fusion-core.pot'],
-      options: {
-          locales: ['de_DE','es_ES', 'fi', 'fr_FR', 'he', 'it_IT', 'ja', 'ko', 'mk_MK', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'ru_RU'],
-          poFilesPath: 'fusion-core/fusion-core-<%= locale%>.po',
-		  msgInit : {
-				cmd : 'msginit',
-				opts : {}
+		},
+		potomo: {
+			theme: {
+				options: { poDel: false },
+				files: [{
+					expand: true,
+					cwd: 'Avada',
+					src: ['*.po'],
+					dest: 'Avada',
+					ext: '.mo',
+					nonull: true
+				}]
 			},
-			msgMerge : {
-				cmd : 'msgmerge',
-				opts : {'no-fuzzy-matching' : true, 'backup' : 'none'}
+			plugin: {
+				options: { poDel: false },
+				files: [{
+					expand: true,
+					cwd: 'fusion-core',
+					src: ['*.po'],
+					dest: 'fusion-core',
+					ext: '.mo',
+					nonull: true
+				}]
 			}
-      }
-    }
-  },
-  potomo: {
-    theme: {
-      options: {
-        poDel: false
-      },
-      files: [{
-        expand: true,
-        cwd: 'Avada',
-        src: ['*.po'],
-        dest: 'Avada',
-        ext: '.mo',
-        nonull: true
-      }]
-    },
-	 plugin: {
-      options: {
-        poDel: false
-      },
-      files: [{
-        expand: true,
-        cwd: 'fusion-core',
-        src: ['*.po'],
-        dest: 'fusion-core',
-        ext: '.mo',
-        nonull: true
-      }]
-    }
-  },
-});
+		},
+	});
 
-grunt.loadNpmTasks( 'grunt-potomo' );
-grunt.loadNpmTasks('grunt-msg-init-merge');
+	grunt.loadNpmTasks( 'grunt-potomo' );
+	grunt.loadNpmTasks( 'grunt-msg-init-merge' );
 
-grunt.registerTask('default', ['msgInitMerge', 'potomo'] );
+	grunt.registerTask('default', ['msgInitMerge', 'potomo'] );
 };
