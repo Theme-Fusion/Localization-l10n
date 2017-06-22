ed7a54000e9af3925eb66e26a71c6dca62cbd360
Aristeides Stathopoulos <aristath@gmail.com>
2017-06-22 11:05:26 +0300

    Add missing languages to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function( grunt ) {
 			theme: {
 				src: ['Avada/Avada.pot'],
 				options: {
-					locales: ['ar', 'ca', 'bg_BG', 'cs_CZ', 'da_DK', 'de_DE', 'el', 'es_ES', 'fa', 'fa_IR', 'fi', 'fr_FR', 'he', 'hr_HR', 'hu_HU', 'it_IT', 'ja', 'ko', 'mk_MK', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'pt_PT', 'ro', 'ru_RU', 'sv_SE', 'tr_TR', 'uk_UA', 'zh_CN', 'zh_TW' ],
+					locales: ['af', 'ak', 'sq', 'am', 'ar', 'hy', 'rup_MK', 'as', 'az', 'az_TR', 'ba', 'eu', 'bel', 'bn_BD', 'bs_BA', 'bg_BG', 'my_MM', 'ca', 'bal', 'zh_CN', 'zh_HK', 'zh_TW', 'co', 'hr', 'cs_CZ', 'da_DK', 'dv', 'nl_NL', 'nl_BE', 'en_US', 'en_AU', 'en_CA', 'en_GB', 'eo', 'et', 'fo', 'fi', 'fr_BE', 'fr_FR', 'fy', 'fuc', 'gl_ES', 'ka_GE', 'de_DE', 'de_CH', 'el', 'gn', 'gu_IN', 'haw_US', 'haz', 'he_IL', 'hi_IN', 'hu_HU', 'is_IS', 'ido', 'id_ID', 'ga', 'it_IT', 'ja', 'jv_ID', 'kn', 'kk', 'km', 'kin', 'ky_KY', 'ko_KR', 'ckb', 'lo', 'lv', 'li', 'lin', 'lt_LT', 'lb_LU', 'mk_MK', 'mg_MG', 'ms_MY', 'ml_IN', 'mr', 'xmf', 'mn', 'me_ME', 'ne_NP', 'nb_NO', 'nn_NO', 'ory', 'os', 'ps', 'fa_IR', 'fa_AF', 'pl_PL', 'pt_BR', 'pt_PT', 'pa_IN', 'rhg', 'ro_RO', 'ru_RU', 'ru_UA', 'rue', 'sah', 'sa_IN', 'srd', 'gd', 'sr_RS', 'sd_PK', 'si_LK', 'sk_SK', 'sl_SI', 'so_SO', 'azb', 'es_AR', 'es_CL', 'es_CO', 'es_MX', 'es_PE', 'es_PR', 'es_ES', 'es_VE', 'su_ID', 'sw', 'sv_SE', 'gsw', 'tl', 'tg', 'tzm', 'ta_IN', 'ta_LK', 'tt_RU', 'te', 'th', 'bo', 'tir', 'tr_TR', 'tuk', 'ug_CN', 'uk', 'ur', 'uz_UZ', 'vi', 'wa', 'cy'],
 					poFilesPath: 'Avada/Avada-<%= locale%>.po',
 					msgInit: {
 						cmd: 'msginit',
@@ -24,7 +24,7 @@ module.exports = function( grunt ) {
 			plugin: {
 				src: ['fusion-core/fusion-core.pot'],
 				options: {
-					locales: ['de_DE', 'es_ES', 'fi', 'fr_FR', 'he', 'it_IT', 'ja', 'ko', 'mk_MK', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'ru_RU', 'sv_SE'],
+					locales: ['af', 'ak', 'sq', 'am', 'ar', 'hy', 'rup_MK', 'as', 'az', 'az_TR', 'ba', 'eu', 'bel', 'bn_BD', 'bs_BA', 'bg_BG', 'my_MM', 'ca', 'bal', 'zh_CN', 'zh_HK', 'zh_TW', 'co', 'hr', 'cs_CZ', 'da_DK', 'dv', 'nl_NL', 'nl_BE', 'en_US', 'en_AU', 'en_CA', 'en_GB', 'eo', 'et', 'fo', 'fi', 'fr_BE', 'fr_FR', 'fy', 'fuc', 'gl_ES', 'ka_GE', 'de_DE', 'de_CH', 'el', 'gn', 'gu_IN', 'haw_US', 'haz', 'he_IL', 'hi_IN', 'hu_HU', 'is_IS', 'ido', 'id_ID', 'ga', 'it_IT', 'ja', 'jv_ID', 'kn', 'kk', 'km', 'kin', 'ky_KY', 'ko_KR', 'ckb', 'lo', 'lv', 'li', 'lin', 'lt_LT', 'lb_LU', 'mk_MK', 'mg_MG', 'ms_MY', 'ml_IN', 'mr', 'xmf', 'mn', 'me_ME', 'ne_NP', 'nb_NO', 'nn_NO', 'ory', 'os', 'ps', 'fa_IR', 'fa_AF', 'pl_PL', 'pt_BR', 'pt_PT', 'pa_IN', 'rhg', 'ro_RO', 'ru_RU', 'ru_UA', 'rue', 'sah', 'sa_IN', 'srd', 'gd', 'sr_RS', 'sd_PK', 'si_LK', 'sk_SK', 'sl_SI', 'so_SO', 'azb', 'es_AR', 'es_CL', 'es_CO', 'es_MX', 'es_PE', 'es_PR', 'es_ES', 'es_VE', 'su_ID', 'sw', 'sv_SE', 'gsw', 'tl', 'tg', 'tzm', 'ta_IN', 'ta_LK', 'tt_RU', 'te', 'th', 'bo', 'tir', 'tr_TR', 'tuk', 'ug_CN', 'uk', 'ur', 'uz_UZ', 'vi', 'wa', 'cy'],
 					poFilesPath: 'fusion-core/fusion-core-<%= locale%>.po',
 					msgInit: {
 						cmd: 'msginit',
@@ -42,7 +42,7 @@ module.exports = function( grunt ) {
 			plugin2: {
 				src: ['fusion-builder/fusion-builder.pot'],
 				options: {
-					locales: ['de_DE', 'es_ES', 'fi', 'fr_FR', 'he', 'it_IT', 'ja', 'ko', 'mk_MK', 'nb_NO', 'nl_NL', 'pl_PL', 'pt_BR', 'ru_RU', 'sv_SE'],
+					locales: ['af', 'ak', 'sq', 'am', 'ar', 'hy', 'rup_MK', 'as', 'az', 'az_TR', 'ba', 'eu', 'bel', 'bn_BD', 'bs_BA', 'bg_BG', 'my_MM', 'ca', 'bal', 'zh_CN', 'zh_HK', 'zh_TW', 'co', 'hr', 'cs_CZ', 'da_DK', 'dv', 'nl_NL', 'nl_BE', 'en_US', 'en_AU', 'en_CA', 'en_GB', 'eo', 'et', 'fo', 'fi', 'fr_BE', 'fr_FR', 'fy', 'fuc', 'gl_ES', 'ka_GE', 'de_DE', 'de_CH', 'el', 'gn', 'gu_IN', 'haw_US', 'haz', 'he_IL', 'hi_IN', 'hu_HU', 'is_IS', 'ido', 'id_ID', 'ga', 'it_IT', 'ja', 'jv_ID', 'kn', 'kk', 'km', 'kin', 'ky_KY', 'ko_KR', 'ckb', 'lo', 'lv', 'li', 'lin', 'lt_LT', 'lb_LU', 'mk_MK', 'mg_MG', 'ms_MY', 'ml_IN', 'mr', 'xmf', 'mn', 'me_ME', 'ne_NP', 'nb_NO', 'nn_NO', 'ory', 'os', 'ps', 'fa_IR', 'fa_AF', 'pl_PL', 'pt_BR', 'pt_PT', 'pa_IN', 'rhg', 'ro_RO', 'ru_RU', 'ru_UA', 'rue', 'sah', 'sa_IN', 'srd', 'gd', 'sr_RS', 'sd_PK', 'si_LK', 'sk_SK', 'sl_SI', 'so_SO', 'azb', 'es_AR', 'es_CL', 'es_CO', 'es_MX', 'es_PE', 'es_PR', 'es_ES', 'es_VE', 'su_ID', 'sw', 'sv_SE', 'gsw', 'tl', 'tg', 'tzm', 'ta_IN', 'ta_LK', 'tt_RU', 'te', 'th', 'bo', 'tir', 'tr_TR', 'tuk', 'ug_CN', 'uk', 'ur', 'uz_UZ', 'vi', 'wa', 'cy'],
 					poFilesPath: 'fusion-builder/fusion-builder-<%= locale%>.po',
 					msgInit: {
 						cmd: 'msginit',
